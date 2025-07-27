62c947361d76b86b7e506970fb50893189b0f80b
Tranmyuyen6 <myuyen120806@gmail.com>
2025-07-27 08:58:08 +0700

    fix

--- buoi3/src/Practice_in_class/Bai3.java
+++ buoi3/src/Practice_in_class/Bai3.java
@@ -1,4 +1,19 @@
 package Practice_in_class;
 
+import java.util.Scanner;
+
 public class Bai3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào một chuỗi: ");
+        String a = sc.nextLine();
+        String b = a.trim();
+        System.out.println("Xoá các khoảng trắng ở đầu và cuối chuỗi: "+b);
+        String c="";
+        for(int i=b.length()-1;i>=0;i--){
+            c+=b.charAt(i);
+        }
+        boolean madam = b.equalsIgnoreCase(c);
+        System.out.println("chuỗi là palindrome: "+madam);
+    }
 }
